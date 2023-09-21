49302ee5ccf6636ee2e5d57a8b1ae029512326d5
Matheus Marques <matheusmarquesneves12@gmail.com>
2023-09-21 07:47:51 -0300

    feat: 🎸 Recipes page - Desktop
    ✅ Closes: #18444

--- apps/front-end/src/pages/recipeList/recipe.tsx
+++ apps/front-end/src/pages/recipeList/recipe.tsx
@@ -2,18 +2,19 @@ import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
 import timer from "../../assets/images/relogio.png"
 import like from "../../assets/images/like.png"
 import liked from "../../assets/images/likeInteragido.png"
-import person1 from "../../assets/images/person1.png"
-import person2 from "../../assets/images/person2.png"
-import person3 from "../../assets/images/person3.png"
+import tt from "../../assets/images/coração.png"
+import { Link } from 'react-router-dom';
 
 
 interface RecipeProps {
+    id: string; // id da receita
     image: string;
     imageAlt: string;
     title: string;
     category: string;
     owner: string;
-    time: string;
+    hours: number;
+    minutes: number;
     description: string;
     moreLikes?: number;
     person1?: string;
@@ -33,38 +34,59 @@ const recipe: React.FC<RecipeProps> = props => {
         if (!description.endsWith('"')) {
           description = description + '"';
         }
-        return description;
+        return description[0] + description.charAt(1).toUpperCase() + description.slice(2).toLowerCase();
     }
 
+    function formattedMinutes(minutes: number): string{
+        return minutes.toString().padStart(2, '0');
+    } 
+
+    const recipeURL = `/receitas/${props.id}`
+
     return (
-        <div className='w-1/3 p-8'>
-            <div className='rounded-md overflow-hidden'>
-                <img src = {props.image} alt = {props.imageAlt} />
-            </div>
+        <div className='w-1/3 p-8 flex flex-col' >
+            <Link to={recipeURL}>
+                <div className='rounded-md overflow-hidden'>
+                    <img src = {props.image} alt = {props.imageAlt} />
+                </div>
+            </Link>
             <h1 className='text-color-orange font-medium'>{transformCase(props.title)}</h1>
             <p className='text-color-gray '>{props.category.toUpperCase()}</p>
             <div className='flex justify-between'>
                 <h2 className='text-black'>{transformCase(props.owner)}</h2>
                 <div className='flex'>
                     <img src= {timer} alt = "tempo estimado da receita" className='h-4 w-4 self-center mr-1'/>
-                    <h2>{props.time}</h2>
+                    <h2 className='m-0'>{props.hours}h{formattedMinutes(props.minutes)}min</h2>
                 </div>
             </div>
             <p className='text-color-gray'>{formatDescription(props.description)}</p>
-            <div className='flex justify-between self-end'>
+            <div className='flex justify-between mt-auto pt-8'>
                 <img src= {liked} />
                 <div className='flex'>
                     {props.person1 !== undefined && (
-                        <img src= {props.person1} alt="" />
+                        <div className='w-6 h-6 rounded-full overflow-hidden flex items-center justify-center'>
+                            <img src= {props.person1} alt="" className='w-full h-full object-cover'/>
+                        </div>
                     )}
                     {props.person2 !== undefined && (
-                        <img src= {props.person2} alt="" />
+                        <div className='w-6 h-6 rounded-full overflow-hidden flex items-center justify-center -ml-2'>
+                            <img src= {props.person2} alt="" className='w-full h-full object-cover'/>
+                        </div>
                     )}
                     {props.person3 !== undefined && (
-                        <img src= {props.person3} alt="" />
+                        <div className='w-6 h-6 rounded-full overflow-hidden flex items-center justify-center -ml-2'>
+                            <img src= {props.person3} alt="" className='w-full h-full object-cover'/>
+                        </div>
                     )}
                     {props.moreLikes !== undefined && (
-                        <div><span>+{props.moreLikes}</span></div>
+                        <div className='w-6 h-6 rounded-full -ml-2 drop-shadow-[0_2px_4px_rgba(0,0,0,0.25)] bg-white'>
+                            <span className='moreLikes w-full h-full flex items-center justify-start'>+{props.moreLikes}</span>
+                        </div>
+                    )}
+                    {props.person1 !== undefined && (
+                        <div className='self-end mt-4 -ml-2 relative z-2'>
+                            <img src= {tt} alt="" />
+                        </div>
                     )}
                 </div>
             </div>
--- apps/front-end/src/pages/recipeList/recipeList.tsx
+++ apps/front-end/src/pages/recipeList/recipeList.tsx
@@ -110,41 +110,29 @@ const recipeList: React.FC = () => {
         </div>
       </div>
       <div className="w-full h-full flex flex-wrap">
-        <Recipe image = {prato1} imageAlt='foto representando o prato Pizza margherita' title='pizza marGherIta' category='ITALIANO' owner='fabiana' time='0h50min' 
-        description='"Receita de pizza Margherita deliciosa e fácil para reunir a família e apreciar com gosto!"' 
-        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {50} />
+        <Recipe image = {prato1} imageAlt='foto representando o prato Pizza margherita' title='pizza marGherIta' category='ITALIANO' owner='fabiana' hours={0} minutes={50} 
+        description='"Receita de PIZZA Margherita deliciosa e fácil para reunir a família e apreciar com gosto!"' 
+        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {50} id='teste1'/>
 
-        <Recipe image = {prato2} imageAlt='foto representando o prato Sopa de Rámen' title='Sopa de Rámen' category='CHINÊS' owner='Yuri' time='1h30min' 
+        <Recipe image = {prato2} imageAlt='foto representando o prato Sopa de Rámen' title='Sopa de Rámen' category='CHINÊS' owner='Yuri' hours={1} minutes={30} 
         description='Diferente e delicioso!'
-        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {42} />
+        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {42} id='teste2'/>
 
-        <Recipe image = {prato3} imageAlt='foto representando o prato shakshuka' title='shakshuka' category='ORIENTE MÉDIO' owner='Thiago' time='0h05min' 
+        <Recipe image = {prato3} imageAlt='foto representando o prato shakshuka' title='shakshuka' category='ORIENTE MÉDIO' owner='Thiago' hours={0} minutes={5} 
         description='Rápido e sofisticado.' 
-        person1 = {person1} person2 = {person2} person3 = {person3}moreLikes = {90} />
+        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {90} id='teste3'/>
 
-        <Recipe image = {prato4} imageAlt='foto representando o prato Torta de carne com ovo' title='Torta de carne com ovo' category='GREGO' owner='Marcela' time='1h10min' 
+        <Recipe image = {prato4} imageAlt='foto representando o prato Torta de carne com ovo' title='Torta de carne com ovo' category='GREGO' owner='Marcela' hours={1} minutes={10} 
         description='Diferente e delicioso!' 
-        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {26} />
+        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {26} id='teste4'/>
 
-        <Recipe image = {prato5} imageAlt='foto representando o prato Picadinho' title='Picadinho' category='BRASILEIRO' owner='Rafa' time='2h00min' 
+        <Recipe image = {prato5} imageAlt='foto representando o prato Picadinho' title='Picadinho' category='BRASILEIRO' owner='Rafa' hours={2} minutes={0} 
         description='Típico prato brasileiro!' 
-        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {26} />
+        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {26} id='teste5'/>
 
-        <Recipe image = {prato6} imageAlt='foto representando o prato Carne de panela desfiada' title='Carne de panela desfiada' category='CHINÊS' owner='Yuri' time='1h30min'
+        <Recipe image = {prato6} imageAlt='foto representando o prato Carne de panela desfiada' title='Carne de panela desfiada' category='CHINÊS' owner='Yuri' hours={1} minutes={30} 
          description='Diferente e delicioso!' 
-         person1 = {person1} person2 = {person2} />
-
-        <Recipe image = {prato4} imageAlt='foto representando o prato Torta de carne com ovo' title='Torta de carne com ovo' category='GREGO' owner='Marcela' time='1h10min' 
-        description='Diferente e delicioso!' 
-        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {26} />
-
-        <Recipe image = {prato5} imageAlt='foto representando o prato Picadinho' title='Picadinho' category='BRASILEIRO' owner='Rafa' time='2h00min' 
-        description='Típico prato brasileiro!'
-        person1 = {person1} person2 = {person2} person3 = {person3} moreLikes = {26} />
-
-        <Recipe image = {prato6} imageAlt='foto representando o prato Carne de panela desfiada' title='Carne de panela desfiada' category='CHINÊS' owner='Yuri' time='1h30min' 
-        description='Diferente e delicioso!'
-        person1 = {person1} person2 = {person2} />
+         person1 = {person1} person2 = {person2} id='teste6'/>
       </div>
     </div>
   );
