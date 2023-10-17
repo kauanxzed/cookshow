93d091b1f68400a510b554911bdb7cb4f3d42bf7
gustavo.rosa <gustavorosa@alunos.unicesumar.edu.br>
2023-10-16 21:07:35 -0300

    save point

--- apps/front-end/src/pages/mockModal/modal.tsx
+++ apps/front-end/src/pages/mockModal/modal.tsx
@@ -9,22 +9,23 @@ const ModalDefault = () => {
       <>
         <Button onClick={() => props.setOpenModal('default')}>Toggle modal</Button>
         <Modal show={props.openModal === 'default'} onClose={() => props.setOpenModal(undefined)}>
-          <Modal.Body className='flex justify-between'>
-            <div>
-              <div className='rounded-full'>
-
+          <Modal.Body className='flex justify-between p-0'>
+            <div className='background-register-recipe pt-5 flex flex-col items-center justify-center'>
+              <div className='rounded-full w-72 h-72 bg-white border-solid border border-[#FF7A00]'>
               </div>
-              <div>
-                <p>Selecione uma foto do seu dispositivo</p>
-                <label htmlFor="photoRecipe" className="custom-file-upload">
-                  Selecionar foto
-                </label>
-                <input id="photoRecipe" name="photoRecipe" type="file" accept=".jpg, .jpeg, .png" className='invisible'/>
+              <div className='flex flex-col items-center justify-center p-2'>
+                <p className=''>Selecione uma foto do seu dispositivo</p>
+                <div className='bg-[#2D3748] duration-300 hover:bg-[#1d242f] rounded-md w-36 h-10 flex items-center justify-center mt-4'>
+                  <label htmlFor="photoRecipe" className="custom-file-upload text-white text-sm h-full w-full text-center cursor-pointer">
+                    <p className='flex justify-center items-center h-full w-full'>Selecionar</p>
+                  </label>              
+                </div>
+                <input id="photoRecipe" name="photoRecipe" type="file" accept=".jpg, .jpeg, .png" className='hidden'/>
               </div>
             </div>
-            <div className="space-y-6">
+            <div className="space-y-6 p-5 w-full">
               <div>
-                <input type="text" name="recipeName" id="recipeName" placeholder='Escreva o nome da sua receita' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                <input type="text" name="recipeName" id="recipeName" placeholder='Escreva o nome da sua receita' className="bg-gray-50 border-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
               </div>
               {/* ADD RECEITA */}
               <div>
