d24b0e6fe71d313c8b820aef8ae9b55d745d6b9c
kauan Lemes Valerio <ra-20000008-2@alunos.unicesumar.edu.br>
2023-10-23 21:09:25 -0300

    feat: duplicate-ingredient

--- apps/back-end/src/recipe/recipe.controller.ts
+++ apps/back-end/src/recipe/recipe.controller.ts
@@ -18,12 +18,12 @@ export class RecipeController {
   async addRecipeIngredient(
     @Param('recipeId') recipeId: string,
     @Param('ingredientId') ingredientId: number,
-    @Body('portion') portion: number,
+    @Body('portion') portion: number
   ) {
     return await this.recipeService.addRecipeIngredient(
       recipeId,
       ingredientId,
-      portion,
+      portion
     );
   }
 
