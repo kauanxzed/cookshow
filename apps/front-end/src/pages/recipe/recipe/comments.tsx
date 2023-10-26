71e8e186ce744b422a2e0495759f75ea5a7e159a
igor Rafael <ra-20056608-2@alunos.unicesumar.edu.br>
2023-10-25 22:23:06 -0300

    doing

--- apps/front-end/src/pages/recipe/recipe/comments.tsx
+++ apps/front-end/src/pages/recipe/recipe/comments.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import TextareaAutosize from 'react-textarea-autosize';
 
 interface comment {
     commentAuthor: string,
@@ -10,12 +11,62 @@ interface CommentsProps {
 }
 
 const Comments: React.FC<CommentsProps> = (props) => {
+
+  const [comment, setComment] = useState("")
+  const [errors, setErrors] = useState({
+    comment: '',
+  });
+
+  
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const inputValue = comment;
+    if (!inputValue.trim()) {
+      setComment("")
+      handleFieldChange('comment', "O campo deve conter ao menos um caracter")
+    }
+    const hasErrors = Object.values(errors).some((error) => !!error);
+  }
+
+  const handleFieldChange = (fieldName: string, msg: string) => {
+    if (msg) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [fieldName]: msg,
+      }));
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [fieldName]: '',
+      }));
+    }
+  };
+
+
     return (
         <div>
+          <form onSubmit={handleSubmit} className="mb-2">
+            <TextareaAutosize
+                minRows={1}
+                maxRows={2}
+                name="comment"
+                id="comment"
+                placeholder="Adiciona um comentario"
+                className="block w-full break-words bg-gray-100 rounded-lg outline-none focus:ring-0 ring-orange-400 focus:ring-offset-0 border-2 border-transparent focus:border-orange-400"
+                value={comment}
+                onChange={(e) => {
+                    setComment(e.target.value)
+                    handleFieldChange('comment', "")
+                }}
+                required
+              />
+              {errors.comment && <p className='text-red-500'>{errors.comment}</p>}
+              <button type="submit">enviar</button>
+          </form>
           {props.comments.map((comment, index) => (
-            <div key={index}>
-              <p>Author: {comment.commentAuthor}</p>
-              <p>Content: {comment.commentContent}</p>
+            <div key={index} className="mb-2 border-b border-solid border-[#e6e6e6]">
+              <p className="text-[#2D3748] font-medium text-sm">{comment.commentAuthor}</p>
+              <p className="flex-wrap break-words">{comment.commentContent}</p>
             </div>
           ))}
         </div>
--- apps/front-end/src/pages/recipe/recipe/recipe.tsx
+++ apps/front-end/src/pages/recipe/recipe/recipe.tsx
@@ -18,7 +18,7 @@ const ingredients = [
 
 const ModalDefault = () => {
   const [openModal, setOpenModal] = useState<string | undefined>();
-  const [commentsVisible, setCommentsVisible] = useState(false);
+  const [commentsVisible, setCommentsVisible] = useState(true);
   const props = { openModal, setOpenModal };
   const recipeMock = {
     recipePhoto: RecipePhotoMock,
@@ -84,63 +84,68 @@ const ModalDefault = () => {
       <Modal
         show={props.openModal === 'default'}
         onClose={() => props.setOpenModal(undefined)}
+        size="5xl"
       >
-        <Modal.Body className="flex h-screen flex-col md:flex-row justify-between p-0 bg-white">
-          <div className="bg-gradient-to-r from-[#FF7A00] mr-10 p-5 flex flex-col items-center justify-center rounded-tl-lg">
-            <button
-                className="text-black text-xl self-start"
-                onClick={() => props.setOpenModal(undefined)}
-              >
-                X
-              </button>
-            <div className="rounded-full w-72 h-72 bg-white border-solid border border-[#FF7A00] flex justify-center align-center overflow-hidden">
-              <img
-                  id="photoRecipe"
-                  alt='Foto da receita'
-                  src={recipeMock.recipePhoto}
-                /> 
+        <Modal.Body className="flex flex-col h-[90vh] p-0 md:flex-row justify-between bg-white rounded-t-lg">
+          <div className="bg-gradient-to-r from-[#FF7A00] h-[90vh] p-5 rounded-tl-lg">
+              <button
+                  className="text-black text-xl self-start"
+                  onClick={() => props.setOpenModal(undefined)}
+                >
+                  X
+                </button>
+              <div className="rounded-full w-72 h-72 bg-white border-solid border border-[#FF7A00] flex justify-center align-center overflow-hidden">
+                <img
+                    id="photoRecipe"
+                    alt='Foto da receita'
+                    src={recipeMock.recipePhoto}
+                  /> 
+              </div>
+            </div>
+          <div className="rounded-tr-lg p-3 w-full h-[90vh] flex flex-col justify-between">
+            <div>
+              <p className='text-xl text-[#9C4B00]'>
+                {recipeMock.recipeName}
+              </p>
+              <p className='text-xs text-[#999999]'> 
+              {recipeMock.recipeOrigin}
+              </p>
+              <p className='font-["Poppins"] mt-2 text-base text-[#666565]'> 
+                {recipeMock.recipeAutor}
+              </p>
+              <div className='flex flex-row'>
+                <RecipeInfo info={'🕙 ' + recipeMock.recipeTime}/>
+                <RecipeInfo info={'🍽️ ' + recipeMock.recipeDifficulty}/>
+                <RecipeInfo info={'🔥 ' + recipeMock.recipeKcal + ' Kcal'}/>
+              </div>
+              <div className='flex flex-row items-center'>
+                <RecipeRating rating={recipeMock.recipeRating}/>
+                <p className='ml-1'>{recipeMock.recipeRating}</p>
+              </div>
+              <div className='flex flex-row'>
+                {recipeMock.recipeIngredients.map((ingredient) => {
+                  return (
+                    <Ingredient name={ingredient.ingredientName}/>
+                  )
+                })}
+              </div>
+            </div>
+            <div className='flex flex-row'>
+                <div className="rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
+                <div className="rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
+                <div className="rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
+                <div className="rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
+            </div>
+            <div className='flex flex-row'>
+              <Like/>
+              <p className='cursor-pointer ml-2' onClick={showComments}>{commentsVisible ? "Ocultar todos os comentários" : "Ver todos os comentários"}</p>
+            </div>
+            <div className='h-36 overflow-y-scroll'>
+                {commentsVisible && (
+                  <Comments comments={recipeMock.recipeComments}/>
+                )}
             </div>
           </div>
-          <div className="p-5 w-full flex flex-col">
-            <p className='text-xl text-[#9C4B00]'>
-              {recipeMock.recipeName}
-            </p>
-            <p className='text-xs text-[#999999]'> 
-            {recipeMock.recipeOrigin}
-            </p>
-            <p className='font-["Poppins"] mt-2 text-base text-[#666565]'> 
-              {recipeMock.recipeAutor}
-            </p>
-            <div className='flex flex-row'>
-              <RecipeInfo info={'🕙 ' + recipeMock.recipeTime}/>
-              <RecipeInfo info={'🍽️ ' + recipeMock.recipeDifficulty}/>
-              <RecipeInfo info={'🔥 ' + recipeMock.recipeKcal + ' Kcal'}/>
-            </div>
-            <div className='flex flex-row items-center'>
-              <RecipeRating rating={recipeMock.recipeRating}/>
-              <p className='ml-1'>{recipeMock.recipeRating}</p>
-            </div>
-            <div className='flex flex-row'>
-              {recipeMock.recipeIngredients.map((ingredient) => {
-                return (
-                  <Ingredient name={ingredient.ingredientName}/>
-                )
-              })}
-            </div>
-              <div className='flex flex-row'>
-                <div className="mt-20 rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
-                <div className="mt-20 rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
-                <div className="mt-20 rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
-                <div className="mt-20 rounded-full w-6 h-6 bg-white border-solid border border-[#FF7A00]"></div>
-              </div>
-              <div className='mt-20 flex flex-row'>
-                <Like/>
-                <p className='cursor-pointer ml-2' onClick={showComments}>Ver todos os comentários</p>
-              </div>
-              {commentsVisible && (
-                <Comments comments={recipeMock.recipeComments}/>
-              )}
-            </div>
         </Modal.Body>
       </Modal>
     </>
