6bfcb85a0cbaa15b0ad1f30fe630a0740bb3d22e
Gabriel Luiz Bruneli Amadeu Gomes <gabrielgomes@alunos.unicesumar.edu.br>
2023-11-06 20:19:53 -0300

    fix: 🐛 Correcting footer (remove fixed)
    ✅ Closes: #19952

--- apps/front-end/src/components/ui/footer/footer.tsx
+++ apps/front-end/src/components/ui/footer/footer.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full bg-white p-5 text-black">
+    <footer className=" bottom-0 w-full bg-white p-5 text-black">
       <div className="container mx-auto text-center">
         <div className="mb-2">
           © 2023 CookShow - Todos os direitos reservados
