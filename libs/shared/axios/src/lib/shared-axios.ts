e0544d1be0d00afed49ea6c1683b30fc4c7e4a67
kauan Lemes Valerio <ra-20000008-2@alunos.unicesumar.edu.br>
2023-11-17 17:11:36 -0300

    usando rota do deploy

--- libs/shared/axios/src/lib/shared-axios.ts
+++ libs/shared/axios/src/lib/shared-axios.ts
@@ -4,7 +4,7 @@ const token =
   localStorage.getItem('jwtToken') || sessionStorage.getItem('jwtToken')
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://cook-show-056b96634c68.herokuapp.com/api/auth',
+  baseURL: 'https://cook-show-056b96634c68.herokuapp.com',
   timeout: 5000,
   headers: {
     Authorization: `Bearer ${token}`,
