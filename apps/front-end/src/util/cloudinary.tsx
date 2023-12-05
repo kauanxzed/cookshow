33c123991031a8102153acba9aacd7abf3fb5792
Kauan Valerio <44176179+kauanxzed@users.noreply.github.com>
2023-12-05 17:08:40 -0300

    Update cloudinary.tsx

--- apps/front-end/src/util/cloudinary.tsx
+++ apps/front-end/src/util/cloudinary.tsx
@@ -3,9 +3,9 @@ import { Cloudinary } from '@cloudinary/url-gen'
 const cld = new Cloudinary({ cloud: { cloudName: 'dknnb0dnz' } })
 cld.setConfig({
   cloud: {
-    cloudName: 'dknnb0dnz',
-    apiKey: '863621582139952',
-    apiSecret: 'f1Ozo067ieetflgKMYkGepALNwc',
+    cloudName: process.env.CLOU_NAME,
+    apiKey: process.env.CLOU_KEY,
+    apiSecret: process.env.CLOU_SECRET,
   },
 })
 export { cld }
